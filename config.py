ea5ecd039b4a3fe62aeb660d052ebc224a78b5c3
textstorm <jiangzhewise@gmail.com>
2017-11-12 20:10:53 +0800

    config update
    add gamma episilon

--- config.py
+++ config.py
@@ -6,12 +6,14 @@ def get_args():
   parser = argsparse.ArgumentParser()
   parser.add_argument('--random_seed', type=int, default='1013', help='random seed')
 
-  parser.add_argument('--batch_size', type=int, default=32, help='batch size')
   parser.add_argument('--hiddens_size', type=int, default=[64], help='mlp hidden size')
   parser.add_argument('--memory_size', type=int, default=50000, helo="experience replay memory size")
 
   parser.add_argument('--max_step', type=int, default=1, help='number of epoch')
-  parser.add_argument('--epsilon', type=float, default=1.0, help='')
+  parser.add_argument('--start_epsilon', type=float, default=1.0, help='')
+  parser.add_argument('--final_epsilon', type=float, default=0.05, help='')
+  parser.add_argument('--batch_size', type=int, default=32, help='batch size')
+  parser.add_argument('--gamma', type=float, default=0.9, help='discount foctor')
   parser.add_argument('--learning_rate', type=float, default=0.01, help='learning rate')
   parser.add_argument('--max_grad_norm', type=float, default=10.0, help='max norm of gradient')
 
