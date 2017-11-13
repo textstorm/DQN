7f78496bc455c0a9fbddef93ff375081501057b7
textstorm <jiangzhewise@gmail.com>
2017-11-13 15:09:54 +0800

    config debug

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 
-import argsparse
+import argparse
 
 def get_args():
 
-  parser = argsparse.ArgumentParser()
+  parser = argparse.ArgumentParser()
   parser.add_argument('--random_seed', type=int, default='1013', help='random seed')
   parser.add_argument('--env_name', type=str, default='CartPole-v0', help='name of environment')
 
   parser.add_argument('--hiddens_size', type=int, default=[64], help='mlp hidden size')
-  parser.add_argument('--memory_size', type=int, default=50000, helo="experience replay memory size")
+  parser.add_argument('--memory_size', type=int, default=50000, help="experience replay memory size")
 
   parser.add_argument('--nb_episodes', type=int, default=1, help='number of episode')
   parser.add_argument('--test_episodes', type=int, default=5, help='number of test episode')
