ccb79b5b0cd356c9c60007e7cb02d9d8249993bd
textstorm <jiangzhewise@gmail.com>
2017-11-14 18:35:54 +0800

    dqn first version

--- dqn.py
+++ dqn.py
@@ -5,15 +5,23 @@ import tensorflow as tf
 import tensorflow.contrib.layers as layers
 from collections import deque
 
-class DeepQ():
-  def __init__(self, args, session, name):
-    self.observation_space_shape = args.observation_space_shape
-    self.num_actions = args.action_space_n
+class LinearSchedule(object):
+  def __init__(self, schedule_timesteps, final_p, initial_p=1.0):
+    self.schedule_timesteps = schedule_timesteps
+    self.final_p = final_p
+    self.initial_p = initial_p
+
+  def value(self, t):
+    interval = min(float(t) / self.schedule_timesteps, 1.0)
+    return self.initial_p - interval * (self.initial_p - self.final_p)
+
+class DeepQ(object):
+  def __init__(self, args, env, name):
+    self.observation_space_shape = env.observation_space.shape[0]
+    self.num_actions = env.action_space.n
     self.learning_rate = args.learning_rate
-    self.sess = session
     self.replay_memory = deque()
 
-    self.hiddens = args.hiddens_size
     self.memory_size = args.memory_size
     self.epsilon = args.start_epsilon
     self.start_epsilon = args.start_epsilon
@@ -22,111 +30,97 @@ class DeepQ():
     self.gamma = args.gamma
     self.max_grad_norm = args.max_grad_norm
 
-    # self.optimizer = tf.train.AdamOptimizer(self.learning_rate)
-    # self.global_step = tf.get_variable(
-    #   'global_step', [], 'int32', tf.constant_initializer(0), trainable=False)
+    self.time_step = 0
+    self.schedule_timesteps = args.schedule_timesteps
+    self.exploration = LinearSchedule(self.schedule_timesteps, 0.1)
+
+    self.sess = tf.InteractiveSession()
 
     self.add_placeholder()
-    self.build_graph()
+    self.build_Q()
     self.build_train()
 
+    self.sess.run(tf.global_variables_initializer())
+
   def add_placeholder(self):
-    self.observ_t = tf.placeholder(tf.float32, 
-                                   [None, self.observation_space_shape], 
-                                   name="observe_t")
+    self.state_t = tf.placeholder(tf.float32, 
+                                  [None, self.observation_space_shape], 
+                                  name="state_t")
     self.action_t = tf.placeholder(tf.float32,
                                    [None, self.num_actions],
                                    name="action_t")
-    # self.reward_t = tf.placeholder(tf.float32,
-    #                                [None],
-    #                                name='reward_t')
-    self.y_t = tf.placeholder(tf.float32,
-                              [None, self.num_actions],
-                              name='y_t')
+    self.y_input = tf.placeholder(tf.float32,
+                                  [None],
+                                  name='y_t')
 
-  def build_graph(self):
-    self.q_values = self.mlp()
-    # action_t = self.egreedy_action(q_values)
-    # new_ob, reward, done, _ = self.env.step(action_t)
-    # if done: reward = 0.
-    # self.replay_memory.append((new_ob, reward, done))
-
-  def mlp(self):
+  def build_Q(self):
     W1 = self._weight_variable([self.observation_space_shape, 20], 'W1')
     b1 = self._bias_variable([20], 'b1')
     W2 = self._weight_variable([20, self.num_actions], 'W2')
     b2 = self._bias_variable([self.num_actions], 'b2')
-    h_layer = tf.nn.relu(tf.matmul(self.observ_t, W1) + b1)
-    return tf.matmul(h_layer, W2) + b2
+    h_layer = tf.nn.relu(tf.matmul(self.state_t, W1) + b1)
 
-  # def egreedy_action(self, observ_t):
-  #   random_action = tf.random_uniform(
-  #     [1], maxval=self.num_actions, minval=0, dtype=tf.int32)
-  #   deterministic_action = tf.argmax(self.q_values, 1)
-  #   choose_action = tf.random_uniform(
-  #     [1], maxval=1, minval=0, dtype=tf.float32) < self.epsilon
+    self.Q_value = tf.matmul(h_layer, W2) + b2
 
-  #   self.epsilon -= (self.start_epsilon - self.final_epsilon) / 10000
-  #   if self.epsilon <= self.final_epsilon:
-  #     self.epsilon = self.final_epsilon
-  #   action = tf.where(choose_action, random_action, deterministic_action)
-  #   return self.sess.run(action, feed_dict={self.observ_t: observ_t})
+  def build_train(self):
+    action_Q = tf.reduce_sum(self.Q_value * self.action_t, 1)
+    self.loss_op = tf.reduce_mean(tf.square(self.y_input - action_Q))
+    self.optimizer = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss_op)
 
-  def egreedy_action(self, observ_t):
-    q_values_t = self.sess.run(self.q_values, feed_dict={self.observ_t: observ_t})
+  def egreedy_policy(self, state):
+    self.epsilon = self.exploration.value(self.time_step)
+    Q_value = self.Q_value.eval(
+      feed_dict={self.state_t: [state]})[0]
+
     random_action = random.randint(0, self.num_actions - 1)
-    deterministic_action = np.argmax(q_values_t)
-    if np.random.uniform(0, 1, 1) < self.epsilon:
+    deterministic_action = np.argmax(Q_value)
+
+    if random.random() <= self.epsilon:
       action = random_action
+      # self.epsilon -= (self.start_epsilon - self.final_epsilon) / 10000
+      # if self.epsilon <= self.final_epsilon:
+      #   self.epsilon = self.final_epsilon
     else:
       action = deterministic_action
-
-    self.epsilon -= (self.start_epsilon - self.final_epsilon) / 10000
-    if self.epsilon <= self.final_epsilon:
-      self.epsilon = self.final_epsilon
-
+    self.time_step += 1
     return action
 
-  def train(self, obs, action, reward, new_obs, done):
-    one_hot_action = tf.one_hot(action, self.num_actions)
-    self.replay_memory.append((obs, action, reward, new_obs, done))
+  def train(self, state, action, reward, next_state, done):
+    one_hot_action = np.zeros(self.num_actions)
+    one_hot_action[action] = 1
+    self.replay_memory.append((state, one_hot_action, reward, next_state, done))
     if len(self.replay_memory) > self.memory_size:
       self.replay_memory.popleft()
+
     if len(self.replay_memory) > self.batch_size:
       self.train_Q()
 
   def train_Q(self):
+    self.time_step += 1
     batch_data = random.sample(self.replay_memory, self.batch_size)
-    obs_batch = [data[0] for data in batch_data]
-    act_batch = [data[1] for data in batch_data]
-    rew_batch = [data[2] for data in batch_data]
-    nobs_batch = [data[3] for data in batch_data]
-    done_batch = [data[4] for data in batch_data]
+    state_batch = [data[0] for data in batch_data]
+    action_batch = [data[1] for data in batch_data]
+    reward_batch = [data[2] for data in batch_data]
+    next_state_batch = [data[3] for data in batch_data]
 
     y_batch = []
-    Q_value_batch = self.sess.run(self.q_values, feed_dict={self.observ_t: nobs_batch})
+    Q_left_batch = self.Q_value.eval(feed_dict={self.state_t: next_state_batch})
 
     for i in range(0, self.batch_size):
       done = batch_data[i][4]
       if done:
-        y_batch.append(rew_batch[i])
+        y_batch.append(reward_batch[i])
       else :
-        y_batch.append(rew_batch[i] + self.gamma * np.max(Q_value_batch[i]))
+        y_batch.append(reward_batch[i] + self.gamma * np.max(Q_left_batch[i]))
 
-    y_batch = self.sess.run(tf.one_hot(y_batch, self.num_actions))
-    self.optimizer.run(feed_dict={
-      self.y_t:y_batch,
-      self.action_t:act_batch,
-      self.observ_t:obs_batch})
+    self.optimizer.run(
+      feed_dict={self.y_input: y_batch,
+                 self.action_t: action_batch,
+                 self.state_t: state_batch})
 
-  def build_train(self):
-    q_value = tf.reduce_sum(self.q_values * self.action_t, 1)
-    self.loss_op = tf.reduce_mean(tf.square(self.y_t - q_value))
-    self.optimizer = tf.train.AdamOptimizer(0.0001).minimize(self.loss_op)
-
-  def action(self,state):
-    return np.argmax(self.sess.run(
-      self.q_values, feed_dict = {self.observ_t:[state]})[0])
+  def action(self, state):
+    return np.argmax(self.Q_value.eval(
+      feed_dict = {self.state_t:[state]})[0])
 
   def _weight_variable(self, shape, name, initializer=None):
     initializer = tf.truncated_normal_initializer(stddev=0.1)
