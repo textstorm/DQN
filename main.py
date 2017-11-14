539d548dbd6f9a86441a30d4082c6c915f296d77
textstorm <jiangzhewise@gmail.com>
2017-11-14 18:36:09 +0800

    main first version

--- main.py
+++ main.py
@@ -7,35 +7,36 @@ import tensorflow as tf
 
 def main(args):
   env = gym.make(args.env_name)
-  args.observation_space_shape = env.observation_space.shape[0]
-  args.action_space_n = env.action_space.n
+  model = dqn.DeepQ(args, env, name='dqn')
 
-  with tf.Session() as sess:
-    model = dqn.DeepQ(args, sess, name='dqn')
-    sess.run(tf.global_variables_initializer())
-    for episode in range(args.nb_episodes):
-      obs = env.reset()
-      for step in range(args.nb_steps):
-        #action = model.egreedy_action(np.array(obs)[None])[0]
-        action = model.egreedy_action([obs])
-        new_obs, reward, done, _ = env.step(action)
-        if done: reward = 0.
-        model.train(obs, action, reward, new_obs, done)
-        obs = new_obs
-        if done: break
+  for episode in range(args.nb_episodes):
+    state = env.reset()
+    for step in range(args.nb_steps):
+      #action = model.egreedy_action(np.array(obs)[None])[0]
+      action = model.egreedy_policy(state)
+      next_state, reward, done, _ = env.step(action)
+      model.train(state, action, reward, next_state, done)
+      state = next_state
+      if done: break
 
-      if episode % 1000 == 0:
-        total_reward = 0
-        for i in range(args.test_episodes):
-          obs = env.reset()
-          for j in range(args.nb_steps):
-            #env.render()
-            action = model.action(obs)
-            state,reward,done,_ = env.step(action)
-            total_reward += reward
-            if done: break
-        ave_reward = total_reward / args.test_episodes
-        print 'episode: ',episode,'Evaluation Average Reward:',ave_reward
+    if episode % 10 == 0:
+      print model.epsilon
+      print len(model.replay_memory)
+      total_reward = 0
+      for i in range(args.test_episodes):
+        state = env.reset()
+        for j in range(args.nb_steps):
+          #env.render()
+          action = model.action(state)
+          state, reward, done, _ = env.step(action)
+          total_reward += reward
+          if done: 
+            break
+
+      ave_reward = total_reward / args.test_episodes
+      print "Episode: %d, Evaluation Average Reward: %.1f" % (episode, ave_reward)
+      if ave_reward >= 200:
+        break
 
 if __name__ == '__main__':
   args = config.get_args()
